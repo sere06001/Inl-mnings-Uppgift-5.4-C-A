827bc0cba360d1e2bd94566cf05e6739c84e0e06
sere06001 <sere06001@utb.vaxjo.se>
2023-11-22 16:38:27 +0100

    Add project files.

---
+++ Inlämnings Uppgift 5.4 C-A/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Uppgift5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in fem namn");
+            string[] arrayNamn = new string[5];
+            for (int i = 0; i < arrayNamn.Length; i++)
+            {
+                arrayNamn[i] = Console.ReadLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Här är namnet som kommer först i bokstavsordning:");
+            Array.Sort(arrayNamn);
+            Console.WriteLine(arrayNamn[0]);
+            Console.ReadKey();
+        }
+    }
+}
